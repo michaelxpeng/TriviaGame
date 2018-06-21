168ef0409f8ca8911bade21cfc47a309751ce45c
mpeng <michaelxpeng@gmail.com>
2018-06-21 03:30:59 -0400

    initial files with pseudocodes

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+// Create global variables
+
+// Create an array of questions with each being an object, containing the questions, the answer options and the answer
+// Create variables for the timer
+// Create empty ariables for the current question, the correct answer, the incorrect answer, the unanswered
+// Create variable that will take the user's choice
+// Create variables for the messages that will display on the result page
+// Create an array of gifs for the corresponding correct answers
+
+// Create a jQuery reference to the start button
+    // Start the game upon click
+
+// When the game starts, reset everything to zero or empty
+
+// Create a function that display the question
+	// Prints the question
+	// Informs the user on the progress of the game
+
+    // Create a for loop through the answer options and add attributes to each
+    // Prints the answer options to the page
+
+    // Start the timer
+
+    // Pauses the time if an answer is registered, have to go inside the newQuestion function
+
+// Create a function for the timer
+    // Set timer to 15 seconds
+    // Print the timer to the page
+    // Prompt the timer to tick down
+
+// Create a function for the running timer
+    // Tick down the clock
+    // Print the timer according to the changing time
+    // If times runs out, end the current question, reset the timer
+
+// Create a function that displays the results page
+    // Display the correct answer
+	// Display the gif according to the question
+	// Compare the user's choice to see if it's correct, incorrect, or did not answer
+    // Tally the scores
+    // Display the message according to the result
+
+// Create a function that counts the final tally
+	// Display the final scores
+
+// Create a restart button
+    // Calls back to the start game function upon click
